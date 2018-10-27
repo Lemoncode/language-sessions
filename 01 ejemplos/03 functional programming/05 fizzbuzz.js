d8500ceabe97b47be383269188e7e76677cb2734
Santiago Camargo Rodríguez <santi.camargo.rodriguez@gmail.com>
2018-10-27 20:03:41 +0200

    Added FizzBuzz description

--- 01 ejemplos/03 functional programming/05 fizzbuzz.js
+++ 01 ejemplos/03 functional programming/05 fizzbuzz.js
@@ -1,4 +1,14 @@
+// EJEMPLO FIZZBUZZ CON RAMDA **********************************************************
+
+// Definición del problema: Implementar una funcionalidad que reciba un número y devuelva una lista
+// de números del 1 al número introducido donde:
+// - Los múltiplos de tres son mostrados como "Fizz"
+// - Los múltiplos de cinco son mostrados como "Buzz"
+// - Los múltiplos de tres y cinco son mostrados como "FizzBuzz"
+// Ejemplo: fizzbuzz(15)
+// Devuelve: [1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz", 11, "Fizz", 13, 14, "FizzBuzz"]
 import {
+  __,
   compose,
   concat,
   curry,
@@ -14,34 +24,26 @@ import {
   not,
   of,
   over,
-  pipe,
   times,
   view,
   when,
   xprod,
-  __,
 } from 'ramda';
 
-// Ejemplo de composición para resolver el ejercicio "fizzbuzz" usando ramda
-
-// La siguiente notación <function_name> :: <arg1> -> <arg2> -> ... -> <argN> -> <return>
-// se lee: <function_name> toma <arg1>, <arg2>, <argN> y devuelve <return>
-
-
-// fizzbuzz(15) [1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz", 11, "Fizz", 13, 14, "FizzBuzz"]
 const isMultipleOf = curry(compose(equals(0), flip(modulo)));
-
 const rule = (predicate, result) => when(
   compose(predicate, view(lensIndex(0))),
   over(lensIndex(1), concat(__, result))
 );
 
 const fizzBuzz = n => map(
-  pipe(
-    rule(isMultipleOf(3), 'Fizz'),
-    rule(isMultipleOf(5), 'Buzz'),
-    filter(compose(not, isEmpty)),
+  compose(
     last,
+    filter(compose(not, isEmpty)),
+    rule(isMultipleOf(5), 'Buzz'),
+    rule(isMultipleOf(3), 'Fizz'),
   ),
   xprod(times(inc, n), of(''))
 );
+
+console.log(fizzBuzz(15));
