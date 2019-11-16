08f1a2e870979ad4edc78e7d2dae0f84c6ea89ea
Javier Calzado <fjcalzadorodriguez@gmail.com>
2019-11-16 19:06:24 +0100

    Preparing webinar utility types

--- 01 ejemplos/03 typescript/06 utility types.ts
+++ 01 ejemplos/03 typescript/06 utility types.ts
@@ -1,11 +1,30 @@
-///-- TIPOS GENÉRICOS DE UTILIDADES *******************************************************
+///-- TIPOS GENÉRICOS DE UTILIDADES ************************************
 
-// *** PARTIAL
+/**
+ * Tipos que operan principalmente sobre interfaces y permiten realizar
+ * cómodamente transformaciones de los mismos.
+ *
+ * A continuación, esquema copy/paste para presentar:
+ */
+
+// *** X ***************
+
+// -- Caso Base --
+
+// -- Caso Práctico --
+
+// -- Definición --
+
+
+
+
+
+// *** PARTIAL ***************
 
 // Convierte en opcionales las propiedades de un interfaz, en la practica
 // esto permite usar implementaciones parciales de un tipo o interfaz:
 
-// - Definition -
+// -- Caso Base --
 interface Person {
   name: string;
   age: number;
@@ -13,117 +32,354 @@ interface Person {
 
 type PartialPerson = Partial<Person>;
 
-// - Use Case -
-interface GlobalState {
-  prop1: string;
-  prop2: number;
-  prop3: boolean;
-}
-
-const createState = <T = any>(initialState: T) => {
+// -- Caso Práctico --
+const createState = <T extends object = any>(initialState: T) => {
   let state: T = initialState;
 
   return {
-    setState: (partialState: Partial<T>): T => {
-      return state = { ...state, ...partialState };
-    },
-  }
-}
+    setState: (partialState: Partial<T>): T =>
+      (state = { ...state, ...partialState })
+  };
+};
 
-const { setState } = createState<GlobalState>({ prop1: "hola", prop2: 43, prop3: true });
+const { setState } = createState({ prop1: "hi", prop2: 43, prop3: true });
 
 console.log(setState({ prop2: 34 }));
 
-// *** REQUIRED
+// -- Definición --
+type MyPartial<T> = {
+  [P in keyof T]?: T[P];
+};
 
-// La contraparte de Partial, convierte en requeridas las propiedades de una
-// interfaz
 
-interface Coords {
+
+
+
+// *** REQUIRED ***************
+
+// La contraparte de Partial, convierte en requeridas las propiedades
+// de una interfaz:
+
+
+// -- Caso Base --
+interface Coord {
   x: number;
   y: number;
   z?: number;
 }
 
-type Coords3D = Required<Coords>;
+type Coord3D = Required<Coord>;
 
-const cs: Coords3D = { // TS: Property 'z' is missing
+// -- Caso Práctico 1  --
+const c3d: Coord3D = { // TS: Property 'z' is missing
   x: 3,
-  y: 0,
+  y: 0
+  // z: 5,
+};
+
+// -- Caso Práctico 2  --
+class Point3D {
+  private coord: Required<Coord>;
+  constructor(coord: Coord) {
+    this.coord = {
+      ...coord,
+      z: coord.z || 0
+    };
+  }
+  getZ() {
+    return this.coord.z;
+  }
+}
+
+const p3d = new Point3D({ x: 1, y: 1 });
+console.log(p3d.getZ());
+
+// -- Definición --
+type MyRequired<T> = {
+  [P in keyof T]-?: T[P];
 };
 
 
-// *** READONLY
 
-// Convierte todas las propiedades en solo lectura:
 
-// - Definition -
+
+// *** READONLY ***************
+
+// Convierte todas las propiedades de un interfaz en solo lectura:
+
+// -- Caso Base --
 interface State {
   username: string;
   password: string;
 }
-type RState = Readonly<State>
 
-// - Use Case -
-const state: State = {
-  username: 'santi',
-  password: 's3cr37'
+type ROState = Readonly<State>;
+
+// También aplicable a arrays:
+
+type ROArray<T> = Readonly<Array<T>>;
+// De hecho existe el tipo ReadonlyArray
+
+// -- Caso Práctico --
+// Un caso muy útil de Readonly se aplica para garantizar que una
+// implementación no va a mutar un objeto determinado. Así, el consumidor
+// de dicha implementación (una función por ejemplo) tiene garantías
+// de que los parámetros de entrada no serán mutados. Pongamos un
+// ejemplo con arrays, donde se ve más claro, aunque lo mismo serviría
+// para objetos:
+
+const sampleArray = [1, 2, 3];
+
+const tailMutable = <T>(array: T[]): T[] => (array.shift(), array);
+
+const tailImmutable = <T>(array: Readonly<T[]>): T[] => {
+  const [, ...tail] = array || [];
+  return tail;
 };
 
-state.password = 'cloud'; // Cannot assign to 'password' because it is a read-only property.
+console.log(sampleArray, tailMutable(sampleArray));
+console.log(sampleArray, tailImmutable(sampleArray));
 
-
-// *** RECORD
-
-// Es un tipo bastante útil para crear tipos de objetos con claves definidas y/o
-// valores definidos
-
-type Cloth = 'shirt' | 'shoes' | 'trousers';
-
-type ClothSizes = Record<Cloth, number>;
-
-const resolutions: ClothSizes = {
-  shirt: 38,
-  trousers: 44,
-  shoes: 41,
+// -- Definición --
+type MyReadonly<T> = {
+  readonly [P in keyof T]: T[P];
 };
 
 
-// *** PICK
 
-// Pick nos permite extraer un subconjunto de propiedades de una interfaz
-interface Employee {
-  firstName: string;
-  surname: string;
-  employeeId: number;
+
+// *** EXCLUDE & EXTRACT ***************
+
+// Utilidades para excluir/extraer elementos comunes en dos uniones.
+// EXCLUDE: excluye de la primera unión los tipos que tiene en común con la 
+// segunda. Por tanto calcula la diferencia matemática.
+// EXTRACT: extrae de la primera unión los tipos que tiene en común con la
+// segunda. Por tanto calcula la intersección matemática.
+
+// -- Caso Base --
+type WeekDay = "lun" | "mar" | "mie" | "jue" | "vie" | "sab" | "dom";
+type WorkDay = Exclude<WeekDay, "sab" | "dom">;
+type Weekend = Extract<WeekDay, "sab" | "dom" | "x">;
+
+// -- Caso Práctico --
+// Podriamos definir:
+type Diff<A extends object = {}, B extends object = {}> = {
+  [P in Exclude<keyof A, keyof B>]: A[P];
+};
+type Common<A extends object = {}, B extends object = {}> = {
+  [P in Extract<keyof A, keyof B>]: A[P] | B[P];
+};
+
+// Y ponerlo a prueba con:
+interface UserDetails {
+  name: string;
+  id: string;
   age: number;
+  phone: number;
+  married: boolean;
 }
 
-type EmployeeResume = Pick<Employee, 'firstName' | 'surname'>;
+interface UserID {
+  name: string;
+  id: number;
+}
+type UserDiff = Diff<UserDetails, UserID>;  // Check intellisense!
+type UserCommon = Common<UserDetails, UserID>; // Check intellisense!
 
-const employeeResume: EmployeeResume = {
-  firstName: 'John',
-  surname: 'Doe',
-  age: 35, // Object literal may only specify known properties, and 'age' does not exist in type ...
+// Ejemplo de implementación en JS:
+
+// Implementación más compatible
+const calculateCommon = <A extends object, B extends object>(a: A, b: B)
+  : Common<A, B> => {
+  const result = { ...a };
+  for (const key in a) {
+    if (a.hasOwnProperty(key) && !b.hasOwnProperty(key)) delete result[key];
+  }
+  return result;
+};
+
+// Implementación más elegante
+// const calculateCommon2 = <A extends object, B extends object>(a: A, b: B)
+//   : Common<A, B> =>
+//   Object.fromEntries(
+//     Object.entries(a).filter(([prop]) => b.hasOwnProperty(prop))
+//   ) as Common<A, B>;
+
+
+const userId: UserID = {
+  name: "Javier",
+  id: 324374
+};
+const userDetails: UserDetails = {
+  name: "Javier",
+  id: "324374",
+  age: 36,
+  phone: 900900900,
+  married: true
+};
+
+const userCommon = calculateCommon(userId, userDetails);
+console.log(userCommon);
+// userCommon. // Check intellisense!
+
+// -- Definición --
+type MyExclude<T, U> = T extends U ? never : T;
+type MyExtract<T, U> = T extends U ? T : never;
+
+
+
+
+// *** PICK & OMIT ***************
+
+// PICK nos permite generar un sub-interfaz, a partir de un interfaz ya
+// existente, escogiendo las propiedades que queremos del original y
+// tipándolas de igual forma. En definitiva, extrae un subconjunto de
+// propiedades (y sus tipos) de una interfaz para generar otra distinta.
+
+// OMIT es el opuesto de PICK, nos permite generar un interfaz a partir de 
+// otro pero en este caso eliminando las propiedades que no deseamos. Es
+// decir, genera una nueva interfaz excluyendo propiedades de la original.
+
+// -- Caso Base --
+interface EmployeeSummary {
+  name: string;
+  id: string;
+  age: number;
+  phone: number;
+  married?: boolean;  // Now we can safely use optionals.
+}
+type EmployeeID = Pick<EmployeeSummary, "id" | "name">; // Check intellisense!
+type EmployeeDetails = Omit<EmployeeSummary, keyof EmployeeID>; // Check intellisense!
+
+// También podríamos haber redefinido los Diff y Common anteriores como:
+type Diff2<A, B> = Omit<A, Extract<keyof A, keyof B>>;
+type Common2<A, B> = Pick<A, Extract<keyof A, keyof B>>;
+
+type EmployeeCredentials = Common2<EmployeeSummary, EmployeeID>;
+type EmployeeInfo = Diff2<EmployeeSummary, EmployeeID>;
+
+// -- Caso Práctico --
+
+// Versión más espartana.
+const filterObject = <T extends object, K extends keyof T>(
+  o: T,
+  ...filterKeys: K[]
+): Omit<T, K> => {
+  const result = { ...o };
+  filterKeys.forEach(key => delete result[key]);
+  return result;
+};
+
+// Versión más funcional.
+const filterObject2 = <T extends object, K extends keyof T>(
+  o: T,
+  ...filterKeys: K[]
+): Omit<T, K> =>
+  Object.fromEntries(
+    Object.entries(o).filter(([key]) => !filterKeys.includes(key as K))
+  ) as Omit<T, K>;
+
+
+const sampleObj = { a: "A", b: "B", c: "C" };
+const filteredObject = filterObject(sampleObj, "a", "b");
+console.log(filteredObject);
+// filteredObject. // Check intellisense!
+
+// -- Definición --
+type MyPick<T, K extends keyof T> = {
+  [P in K]: T[P];
+};
+type MyOmit<T, K extends keyof any> = Pick<T, Exclude<keyof T, K>>;
+
+
+
+
+// *** RECORD ***************
+
+// Es un tipo bastante útil para armar objetos desde cero a partir de un
+// conjunto de claves definidas a las que asignamos un tipado concreto.
+
+// -- Caso Base --
+type Sizes = "small" | "medium" | "large";
+type EurSizes = Record<Sizes, string>;
+type UkSizes = Record<Sizes, number>;
+
+const eurSizes: EurSizes = { small: "s", medium: "m", large: "l" };
+const ukSizes: UkSizes = { small: 8, medium: 10, large: 12 };
+
+// -- Caso Práctico --
+// Ejemplo más elaborado donde vamos a usar Record para transformar
+// un mismo interfaz de distintas formas.
+interface Inventoriable {
+  id: number;
+  name: string;
+}
+type InventoryByName<T extends Inventoriable> = Record<T["name"], Omit<T, "name">>;
+type InventoryById<T extends Inventoriable> = Record<T["id"], Omit<T, "id">>;
+
+const pivotInventory = <T extends Inventoriable = Inventoriable>(
+  list: InventoryByName<T>
+): InventoryById<T> => {
+  return Object.entries<Omit<Inventoriable, "name">>(list).reduce((result, [name, product]) => {
+    result[product.id as number] = { name, ...filterObject(product, "id") };
+    return result;
+  }, {} as InventoryById<T>);
+};
+
+interface Product extends Inventoriable {
+  stock?: boolean;
+}
+
+const productsByName = {
+  arroz: { id: 7, stock: false },
+  papas: { id: 6, stock: true },
+  jamon: { id: 3, stock: true }
+};
+
+const productsById = pivotInventory<Product>(productsByName);
+
+console.log(productsById);
+// productsById[7]. //Check intellisense!
+
+
+// -- Definición --
+type MyRecord<K extends keyof any, T> = {
+  [P in K]: T;
 };
 
 
-// *** OMIT
-
-// Omit sería el opuesto a Pick: permite crear un tipo con todas las propiedades menos las especificadas
-
-type User = Omit<Employee, 'employeeId'>;
-
-const user: User = {
-  age: 52,
-  firstName: 'Alan',
-  surname: 'Walker',
-};
 
 
-// *** RETURNTYPE
+// *** NON NULLABLE ***************
 
-// De las más recientes adquisiciones (TS 2.8) permite inferir el tipo de dato que devuelve una función
+// NonNullable genera un tipo nuevo a partir de otro, previniendo
+// que su valor pueda ser null o undefined.
 
-const addTimestamp = (user: User) => ({ ...user, timestamp: Date.now() });
-type UserWithTimestamp = ReturnType<typeof addTimestamp>;
+// -- Caso Base --
+type Choice = "left" | "right" | "center" | undefined | null;
+type ValidChoice = NonNullable<Choice>;
+
+// -- Caso Práctico --
+// Tiene sentido en un entorno estricto (strictNullChecks) de lo contrario
+// el compilador siempre permitirá valores a null/undefined para cualquier
+// tipo.
+
+// -- Definición --
+type MyNonNullable<T> = T extends null | undefined ? never : T;
+
+
+
+
+// *** FUNCTION HELPERS ***************
+
+// RETURN TYPE: Permite inferir el tipo de dato que devuelve una función.
+// PARAMETERS: Permite inferir el tipado de los argumentos de entrada de
+// una función en formato tupla.
+
+const addTimestamp = (user: UserID, useLocale: boolean = false) => ({
+  ...user,
+  timestamp: useLocale ? Date().toLocaleString() : Date.now()
+});
+
+type UserWithTimestamp = ReturnType<typeof addTimestamp>; // Check intellisense!
+type Params = Parameters<typeof addTimestamp>; // Check intellisense!
